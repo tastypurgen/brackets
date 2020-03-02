6b9920250c58f47634aae409f015ee6b41041241
Andrei Abramchuk <stilegs@gmail.com>
2020-03-02 21:59:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let currentValue;
+	bracketsConfig.forEach(() => {
+		let count = bracketsConfig.length
+		while (count > 0) {
+			currentValue = bracketsConfig[count - 1].join('')
+			if (str.includes(currentValue)) {
+				str = str.replace(currentValue, '')
+			}
+			else {
+				count--
+			}
+		}
+	})
+	return str.length === 0 ? true : false
 }
